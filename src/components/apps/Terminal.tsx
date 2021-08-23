2dc456003c4461f09abae04b1604bba86b51e6b2
Yelan Tao <yeltao@student.ethz.ch>
2021-08-23 14:59:49 +0200

    delete howdare from terminal

--- src/components/apps/Terminal.tsx
+++ src/components/apps/Terminal.tsx
@@ -2,109 +2,8 @@ import React, { Component } from "react";
 import terminal from "../../configs/terminal";
 import { TerminalData } from "../../types";
 
-const emojis = [
-  "\\(o_o)/",
-  "(˚Δ˚)b",
-  "(^-^*)",
-  "(╯‵□′)╯",
-  "\\(°ˊДˋ°)/",
-  "╰(‵□′)╯"
-];
-
-const getEmoji = () => {
-  return emojis[Math.floor(Math.random() * emojis.length)];
-};
-
-const characters =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789富强民主文明和谐自由平等公正法治爱国敬业诚信友善";
-
-interface HowDareProps {
-  setRMRF: (value: boolean) => void;
-}
-
 interface TerminalState {
-  rmrf: boolean;
   content: JSX.Element[];
-}
-
-// rain animation is adopted from: https://codepen.io/P3R0/pen/MwgoKv
-class HowDare extends Component<HowDareProps> {
-  private $canvas = null as HTMLCanvasElement | null;
-  private ctx = null as CanvasRenderingContext2D | null;
-  private intervalId = null as any;
-  private emoji = getEmoji();
-  private font_size = 12;
-  private drops = [] as number[];
-
-  componentDidMount() {
-    const $container = document.querySelector(
-      "#how-dare-container"
-    ) as HTMLElement;
-    this.$canvas = document.querySelector("#how-dare") as HTMLCanvasElement;
-    this.$canvas.height = $container.offsetHeight;
-    this.$canvas.width = $container.offsetWidth;
-    this.ctx = this.$canvas.getContext("2d");
-
-    const columns = this.$canvas.width / this.font_size;
-    this.drops = [];
-    // x: x coordinate, 1: y-coordinate
-    for (let x = 0; x < columns; x++) this.drops[x] = 1;
-
-    this.intervalId = setInterval(this.rain.bind(this), 33);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalId);
-  }
-
-  rain() {
-    this.ctx = this.ctx as CanvasRenderingContext2D;
-    this.$canvas = this.$canvas as HTMLCanvasElement;
-
-    this.ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-    this.ctx.fillRect(0, 0, this.$canvas.width, this.$canvas.height);
-
-    this.ctx.fillStyle = "#2e9244";
-    this.ctx.font = `${this.font_size}px arial`;
-
-    for (let i = 0; i < this.drops.length; i++) {
-      const text = characters[Math.floor(Math.random() * characters.length)];
-
-      this.ctx.fillText(
-        text,
-        i * this.font_size,
-        this.drops[i] * this.font_size
-      );
-
-      // sends the drop back to the top randomly after it has crossed the screen
-      // adding randomness to the reset to make the drops scattered on the Y axis
-      if (
-        this.drops[i] * this.font_size > this.$canvas.height &&
-        Math.random() > 0.975
-      )
-        this.drops[i] = 0;
-
-      // increments Y coordinate
-      this.drops[i]++;
-    }
-  }
-
-  render() {
-    return (
-      <div
-        id="how-dare-container"
-        className="fixed w-full h-full bg-black text-white"
-        onClick={() => this.props.setRMRF(false)}
-      >
-        <canvas id="how-dare"></canvas>
-        <div className="font-avenir absolute text-center h-28 mx-auto -mt-20 bottom-0 left-0 right-0 top-1/2">
-          <div className="text-4xl">{this.emoji}</div>
-          <div className="text-3xl mt-4">HOW DARE YOU!</div>
-          <div className="mt-4">Click to go back</div>
-        </div>
-      </div>
-    );
-  }
 }
 
 export default class Terminal extends Component<{}, TerminalState> {
@@ -120,8 +19,7 @@ export default class Terminal extends Component<{}, TerminalState> {
   constructor(props: {}) {
     super(props);
     this.state = {
-      content: [],
-      rmrf: false
+      content: []
     };
     this.commands = {
       cd: this.cd,
@@ -256,9 +154,6 @@ export default class Terminal extends Component<{}, TerminalState> {
           <span className="text-red-400">help</span> - Display this help menu
         </li>
         <li>
-          <span className="text-red-400">rm -rf /</span> - :)
-        </li>
-        <li>
           press <span className="text-red-400">up arrow / down arrow</span> -
           Select history commands
         </li>
@@ -314,8 +209,7 @@ export default class Terminal extends Component<{}, TerminalState> {
       // we can't edit the past input
       $input.setAttribute("readonly", "true");
 
-      if (input_text.substr(0, 6) === "rm -rf") this.setState({ rmrf: true });
-      else if (cmd && Object.keys(this.commands).includes(cmd)) {
+      if (cmd && Object.keys(this.commands).includes(cmd)) {
         this.commands[cmd](args);
       } else {
         this.generateResultRow(
@@ -367,10 +261,10 @@ export default class Terminal extends Component<{}, TerminalState> {
       <div key={`terminal-input-row-${id}`} className="w-full h-6 flex">
         <div className="w-max flex items-center">
           <span className="text-yellow-200">
-            zou@macbook-pro{" "}
+            glan@Glanfaloths-Macbook-Pro{" "}
             <span className="text-green-300">{this.getCurDirName()}</span>
           </span>
-          <span className="ml-1.5 text-red-400">{">"}</span>
+          <span className="ml-1.5 text-red-400">{"$"}</span>
         </div>
         <input
           id={`terminal-input-${id}`}
@@ -405,11 +299,6 @@ export default class Terminal extends Component<{}, TerminalState> {
         className="terminal font-terminal relative nightwind-prevent nightwind-prevent-block w-full h-full bg-gray-800 bg-opacity-90 text-white text-sm font-normal overflow-y-scroll"
         onClick={() => this.focusOnInput(this.curInputTimes)}
       >
-        {this.state.rmrf && (
-          <HowDare
-            setRMRF={(value: boolean) => this.setState({ rmrf: value })}
-          />
-        )}
         <div className="w-full h-max pt-2 px-1.5 ">
           <span className="text-green-300">ヽ(ˋ▽ˊ)ノ</span>: Hey, you found the
           terminal! Type `help` to get started.
