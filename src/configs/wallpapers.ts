cb44e4f27542f438c8b3921a2c083d023d7d96d0
Yelan Tao <yeltao@student.ethz.ch>
2021-08-25 15:11:11 +0200

    change wallpaper to big sur

--- src/configs/wallpapers.ts
+++ src/configs/wallpapers.ts
@@ -1,8 +1,8 @@
 import { WallpaperData } from "../types";
 
 const wallpapers: WallpaperData = {
-  day: "img/ui/wallpaper-day.jpg",
-  night: "img/ui/wallpaper-night.jpg"
+  day: "img/ui/wallpaper-day.jpeg",
+  night: "img/ui/wallpaper-night.jpeg"
 };
 
 export default wallpapers;
