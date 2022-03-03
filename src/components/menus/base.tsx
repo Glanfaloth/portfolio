c169362aaefb583688d1ed2003d6c1a77cbc288a
Glanfaloth <yeltao@student.ethz.ch>
2022-03-03 17:28:44 +0100

    work in progress

--- src/components/menus/base.tsx
+++ src/components/menus/base.tsx
@@ -1,35 +1,34 @@
-import React, { Component } from "react";
+import type { ReactNode } from "react";
 
 interface MenuItemProps {
   onClick?: (e: React.MouseEvent<HTMLLIElement>) => void;
+  children: ReactNode;
 }
 
 interface MenuItemGroupProps {
   border?: boolean;
+  children: ReactNode;
 }
+const MenuItem = (props: MenuItemProps) => {
+  return (
+    <li
+      onClick={props.onClick}
+      className="px-5 leading-6 text-black cursor-default hover:bg-blue-500 hover:text-white"
+    >
+      {props.children}
+    </li>
+  );
+};
 
-export class MenuItem extends Component<MenuItemProps, {}> {
-  render() {
-    return (
-      <li
-        onClick={this.props.onClick}
-        className="px-5 leading-6 text-black cursor-default hover:bg-blue-500 hover:text-white"
-      >
-        {this.props.children}
-      </li>
-    );
-  }
-}
+const MenuItemGroup = (props: MenuItemGroupProps) => {
+  return (
+    <ul className="py-1">
+      {props.children}
+      {props.border === true && (
+        <hr className="text-gray-500 opacity-50 my-1 mx-2.5" />
+      )}
+    </ul>
+  );
+};
 
-export class MenuItemGroup extends Component<MenuItemGroupProps, {}> {
-  render() {
-    return (
-      <ul className="py-1">
-        {this.props.children}
-        {this.props.border === true && (
-          <hr className="text-gray-500 opacity-50 my-1 mx-2.5" />
-        )}
-      </ul>
-    );
-  }
-}
+export { MenuItem, MenuItemGroup };
